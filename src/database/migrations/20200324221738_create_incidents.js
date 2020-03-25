4584eaf0351a6f48176c6fdf19f24b8f4e7382f0
renato-junior <renato-bgs@live.com>
2020-03-25 01:08:51 -0300

    Back-end da aplicação

---
+++ src/database/migrations/20200324221738_create_incidents.js
@@ -0,0 +1,22 @@
+exports.up = function (knex) {
+    return knex.schema.createTable('incidents', (table) => {
+        table.increments();
+
+        table.string('title').notNullable();
+        table.string('description').notNullable();
+        table.string('value').notNullable();
+
+        table.string('ong_id').notNullable();
+
+        table.foreign('ong_id').references('id').inTable('ongs');
+
+    })
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('incidents');
+};
+
+
+//Para criat a tabala executar:
+// npx knex migrate:latest
